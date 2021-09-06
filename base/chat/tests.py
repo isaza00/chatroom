916b22a6144082fd00b5d0ed7f00b7d0741fd012
Norman <isaza00@hotmail.com>
2021-09-05 19:29:07 -0500

    adding unit tests

--- base/chat/tests.py
+++ base/chat/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .tasks import get_bot_message
 
-# Create your tests here.
+
+
+class BotTestCase(TestCase):
+
+    def test_get_bot_message(self):
+        """Should return bot meesage from pattern /stock=aapl"""
+        message = "/stock=aapl"
+        bot_message = get_bot_message(message)
+        
+        
+        self.assertIn('AAPL.US quote is $', bot_message)
+        self.assertIn('per share', bot_message)
+
+    def test_get_bot_error_message_on_empty_string(self):
+        """Should return error bot message from pattern /stock="""
+        message = "/stock="
+        bot_message = get_bot_message(message)
+        
+        self.assertIn('There is no data for requested symbol', bot_message)
+
+    def test_get_bot_error_message_on_wrong_string(self):
+        """Should return error bot message from pattern /stock=wrong"""
+        message = "/stock=wrong"
+        bot_message = get_bot_message(message)
+        
+        self.assertIn('There is no data for requested symbol', bot_message)
